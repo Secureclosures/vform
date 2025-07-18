1b2de1e00bc07c47db53e7306cae99bf4b0e847f
Secureclosures <itsupport@secureclosures.com>
2025-07-18 14:46:39 +0530

    robot+sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://vform.in',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: ['/careers'],
+};
